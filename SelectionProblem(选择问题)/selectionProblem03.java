6becccab3f136867b6e287251406cf56713343d8
Zxnaruto <zxnaruto1010@163.com>
2017-12-23 16:25:50 +0800

    JavaDataStructure
    3 methods of selection problem

--- SelectionProblem(选择问题)/selectionProblem03.java
+++ SelectionProblem(选择问题)/selectionProblem03.java
@@ -38,7 +38,7 @@ public class selectionProblem03 {
 		System.out.println("时间：" + (EndTime - StartTime) + "ns");
 
 	}
-    //堆排序
+    //堆排序，就是把最上面的根元素取出，剩下的元素再次构建堆，然后在取出最上面的，直到i=1,取出的序列即为排好序的
 	public static void HeapSort(int[] array, int length) {
 		BuildHeap(array, length);
 		for (int i = length - 1; i >= 1; i--) {
@@ -58,17 +58,17 @@ public class selectionProblem03 {
 	public static void PercolateDown(int[] array, int index, int length) {
 		int min;
 		while (index * 2 + 1 < length) {
-			min = index * 2 + 1;
+			min = index * 2 + 1;//左节点
 			if (index * 2 + 2 < length) {
 				if (array[min] > array[index * 2 + 2]) {
-					min = index * 2 + 2;
+					min = index * 2 + 2;//右节点
 				}
 			}
-			if (array[index] < array[min]) {
+			if (array[index] < array[min]) {//如果左右节点中有一个值大于其根节点则对出
 				break;
 			} else {
-				Swap(array, index, min);
-				index = min;
+				Swap(array, index, min);//把小的子节点与大的（根）交换
+				index = min;//下移一层继续，直到全部完成
 			}
 		}
 	}
